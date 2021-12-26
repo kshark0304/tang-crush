9276b21c6cec7a4577e4ad2f4210d84059351855
kshark0304 <94666705+kshark0304@users.noreply.github.com>
2021-12-26 18:06:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -10,9 +10,9 @@ const CONFIG = {
     btnYes: 'Vẫn cứ là thích anh <33',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnReply: 'Làm người iêu khánh nha <3',
     reply: 'Diễm Yêu Khánh <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu diễm nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
